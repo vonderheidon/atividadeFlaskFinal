fddca4716f05f90af2a2e59a040fb6b1d02d077a
vonderheidon <jefferson.sg91@hotmail.com>
2024-09-17 21:44:54 -0300

    teste deploy

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 import dao
 
 app = Flask(__name__)
-app.secret_key = '123chave'
+app.secret_key = '1234chave'
 
 
 @app.route('/', methods=['GET', 'POST'])
